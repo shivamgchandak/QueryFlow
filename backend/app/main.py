9ac9c0b9119a84e15c477ef4d25d7c87480f0057
Shivam Chandak <shivamgchandak@gmail.com>
2025-12-21 03:35:06 +0530

    cors issue fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://hemut-qna-by-shivam.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
